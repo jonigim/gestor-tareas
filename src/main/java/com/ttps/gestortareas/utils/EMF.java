109eeb778fb669301ce7d84a4f57d6ad5aafa50f
Jonatan N. Gimenez <jonatan.gimenez@despegar.com>
2017-11-10 08:35:54 -0300

    EMF

--- src/main/java/com/ttps/gestortareas/utils/EMF.java
+++ src/main/java/com/ttps/gestortareas/utils/EMF.java
@@ -6,6 +6,10 @@ import javax.persistence.Persistence;
 public class EMF {
 	private	static final EntityManagerFactory EM = Persistence.createEntityManagerFactory("up");
 	
+	private EMF() {
+		//Nothing to do	
+	}
+	
 	public static EntityManagerFactory getEMF() {
 		return EM;
 	}
